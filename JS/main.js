3808bf70e8a6ebea9165ad260313cca1a3ea8b5c
Amr <82913751+Amr-AG@users.noreply.github.com>
2021-12-03 05:09:52 +0200

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -12,22 +12,22 @@ submitClick.addEventListener("click", (e) => {
     let tasks = document.createElement("div");
     tasks.className = "tasks";
     tasks.style.cssText =
-      "min-width:100%; border-radius:5px;min-height: 70px; padding:25px 15px; border-bottom:solid 2px #5a5a5a; display: flex;flex-wrap: no-wrap; background-color:#2e2e2e; color:white; justify-content:space-between;align-items:center;";
+      "width:445px; min-height:290px; border-radius:8px; padding:25px 15px; border-bottom:solid 2px #5a5a5a; display: flex;flex-wrap: wrap; background-color:#2e2e2e; color:white; justify-content:space-between;align-items:start; margin: 25px 15px;";
     //creat content
     let content = document.createElement("span");
+    content.className = "content"
     content.innerHTML = addTask.value;
     content.style.cssText =
-      " font-size:25px; height: 100%; display: flex;align-items: center; padding:0 10px;";
+      "width:210px; font-size:25px; height: 100%; display: flex;align-items: start; padding:0 10px;";
     //creat buttons Delete and Done
     let buttons = document.createElement("div");
+    buttons.className = "buttons";
     buttons.style.cssText =
-      " display:flex; justify-content:space-between; align-items:center;";
+      "width:80px; display:flex; justify-content:space-between;flex-wrap: wrap;align-items:center;";
     //delete button Creat
     let DeleteTask = document.createElement("span");
-    DeleteTask.style.cssText =
-      "padding:5px; margin-right:15px;margin-left:10px;width:80px; height:30px; color:white; background-color:#ff6969de; cursor:pointer; text-align:center; z-index:5; border-radius:5px;";
-    DeleteTask.innerHTML = "Delete";
-
+    DeleteTask.className = "fas fa-minus-circle";
+    DeleteTask.style.cssText = "color:#cc6363; font-size:28px;margin:auto; margin-right:5px;";
     //delete click
     DeleteTask.onclick = () => {
       tasks.style.display = "none";
@@ -35,16 +35,15 @@ submitClick.addEventListener("click", (e) => {
     };
 
     //Done button Creat
-    let DoneTask = document.createElement("span");
-    DoneTask.style.cssText =
-      "margin-left:20px; 10px;padding:5px; width:80px; height:30px; color:white; background-color:#4b8778; cursor:pointer; text-align:center; z-index:5; border-radius:5px;";
-    DoneTask.innerHTML = "Done";
+    let DoneTask = document.createElement("i");
+    DoneTask.className = "fas fa-check-circle";
+    DoneTask.style.cssText = "color:#4b8778; font-size:28px; margin:auto; margin-right:10px;";
 
     //Done click
     DoneTask.onclick = () => {
       tasks.style.backgroundColor = "#4b8778";
       tasks.style.transition = "2s";
-      DoneTask.style.backgroundColor = "#2e2e2e";
+      DoneTask.style.color = "black";
       DoneTask.style.transition = "2s";
       document.getElementById("done").play();
     };
