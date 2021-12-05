42f03d8f5b9e393d2f2df1908d205af2e96bdca0
Amr <82913751+Amr-AG@users.noreply.github.com>
2021-12-06 00:19:24 +0200

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -11,23 +11,16 @@ submitClick.addEventListener("click", (e) => {
     //craet Tasks after submit clicked
     let tasks = document.createElement("div");
     tasks.className = "tasks";
-    tasks.style.cssText =
-      "width:445px; min-height:290px; border-radius:8px; padding:25px 15px; border-bottom:solid 2px #5a5a5a; display: flex;flex-wrap: wrap; background-color:#2e2e2e; color:white; justify-content:space-between;align-items:start; margin: 25px 15px;";
     //creat content
     let content = document.createElement("span");
-    content.className = "content"
+    content.className = "content";
     content.innerHTML = addTask.value;
-    content.style.cssText =
-      "width:210px; font-size:25px; height: 100%; display: flex;align-items: start; padding:0 10px;";
     //creat buttons Delete and Done
     let buttons = document.createElement("div");
     buttons.className = "buttons";
-    buttons.style.cssText =
-      "width:80px; display:flex; justify-content:space-between;flex-wrap: wrap;align-items:center;";
     //delete button Creat
     let DeleteTask = document.createElement("span");
-    DeleteTask.className = "fas fa-minus-circle";
-    DeleteTask.style.cssText = "cursor: pointer;color:#cc6363; font-size:28px;margin:auto; margin-right:5px;";
+    DeleteTask.className = "fas fa-minus-circle delete";
     //delete click
     DeleteTask.onclick = () => {
       tasks.style.display = "none";
@@ -36,8 +29,7 @@ submitClick.addEventListener("click", (e) => {
 
     //Done button Creat
     let DoneTask = document.createElement("i");
-    DoneTask.className = "fas fa-check-circle";
-    DoneTask.style.cssText = "cursor: pointer;color:#4b8778; font-size:28px; margin:auto; margin-right:10px;";
+    DoneTask.className = "fas fa-check-circle done";
 
     //Done click
     DoneTask.onclick = () => {
